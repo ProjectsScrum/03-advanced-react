1213dea46ec3735b280b0b0948bdbfb3d9fad1e5
Nuno Costa <nuno.costa.wk@gmail.com>
2024-02-12 19:07:45 +0000

    feat: create counter app

--- src/tutorial/01-useState/starter/01-error-example.jsx
+++ src/tutorial/01-useState/starter/01-error-example.jsx
@@ -1,5 +1,45 @@
+import {useState} from 'react'
+
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  const [num, setNum] = useState(0);
+ 
+  const handleClickIncrease = () =>{
+    return setNum((num)=> num +1)
+  }
+
+  const handleClickDecrease = () =>{
+    return num > 0 ? setNum((num)=> num -1) : "";
+  }
+  
+  return(
+    <div>
+        <h1>INCREASE VALUE</h1>
+        <h2>{num}</h2>
+        <button type='button' className="btn" onClick={handleClickDecrease}>Decrease</button>
+        <button type='button' className="btn" onClick={handleClickIncrease}>Increase</button>
+    </div>
+  ) 
+  
 };
 
 export default ErrorExample;
+
+/*
+
+const ErrorExample = () => {
+  const [num, setNum] = useState(0);
+
+  const handleClick = () =>{
+    return setNum((num)=> num + 1);
+  }
+
+  return(
+    <div>
+        <h2>useState error example</h2>
+        <button onClick={handleClick}>Click to Increase</button>
+        <p>{num}</p>
+    </div>
+  ) 
+  
+};
+*/
--- src/tutorial/01-useState/starter/03-useState-array.jsx
+++ src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,32 @@
+import React from 'react';
+import {data} from '../../../data';
+import { useState } from "react";
+
+
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = useState(data);
+  
+
+  const handleClickRemove = () =>{
+
+    return 
+  }
+
+  return <div>
+        <h2>useState array example</h2>
+        {
+          people.map((item)=>{
+              const {id, name} = item;
+
+            return <div key={id}>
+                <span>{name}</span>
+                <br/>
+                <button type="button" onClick={handleClickRemove}>Button</button>
+            </div>
+          })
+        }
+
+    </div>
 };
 
 export default UseStateArray;
